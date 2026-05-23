30d9a3a58babf7c440299186590a1229153cb479
Ho Duy Nhat <138421401+duynhat1504@users.noreply.github.com>
2026-05-24 00:03:02 +0700

    Feature/user: merge develop (test) (#2)
    * connect PostgreSQL, JPA config
    
    * feature(user): create entity, controller/service(not yet completed)

---
+++ src/main/java/com/duynhat/ecommerce_backend/modules/user/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.duynhat.ecommerce_backend.modules.user.impl;
+
+import com.duynhat.ecommerce_backend.modules.user.UserRepository;
+import com.duynhat.ecommerce_backend.modules.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    
+}
