ad787f8ba5d8af06db27ee7cdd0b27af492b8b8a
Ho Duy Nhat <hdnhat2005@gmail.com>
2026-07-03 15:33:06 +0700

    feature(product): full-text search name and description

--- src/main/java/com/duynhat/ecommerce_backend/modules/product/ProductRepository.java
+++ src/main/java/com/duynhat/ecommerce_backend/modules/product/ProductRepository.java
@@ -1,13 +1,51 @@
 package com.duynhat.ecommerce_backend.modules.product;
 
 import com.duynhat.ecommerce_backend.modules.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
+    @Query(
+            value = """
+                    SELECT
+                        p.id,
+                        p.name,
+                        p.description,
+                        p.price,
+                        p.stock,
+                        p.image_url,
+                        p.active,
+                        p.category_id,
+                        p.created_at,
+                        p.updated_at
+                    FROM products p
+                    WHERE p.active = true
+                      AND p.search_vector @@ websearch_to_tsquery('simple', :keyword)
+                    ORDER BY
+                        ts_rank_cd(
+                            p.search_vector, websearch_to_tsquery('simple', :keyword) 
+                        ) DESC,
+                        p.created_at DESC                                                            
+                    """,
+            countQuery = """
+                    SELECT COUNT(*)
+                    FROM products p
+                    WHERE p.active = true
+                      AND p.search_vector @@ websearch_to_tsquery('simple', :keyword)
+                    """,
+            nativeQuery = true
+    )
+    Page<Product> searchFullText(
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
     boolean existsProductByNameIgnoreCase(String name);
     Optional<Product> findProductByNameIgnoreCase(String name);
 }
