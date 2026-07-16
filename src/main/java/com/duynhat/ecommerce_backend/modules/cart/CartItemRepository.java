711406cc3cdb614a9e8526ca688b24e6de810d84
Ho Duy Nhat <hdnhat2005@gmail.com>
2026-07-16 08:00:58 +0700

    feature(cart): fix add item and delete item from cart

--- src/main/java/com/duynhat/ecommerce_backend/modules/cart/CartItemRepository.java
+++ src/main/java/com/duynhat/ecommerce_backend/modules/cart/CartItemRepository.java
@@ -2,6 +2,9 @@ package com.duynhat.ecommerce_backend.modules.cart;
 
 import com.duynhat.ecommerce_backend.modules.cart.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +14,17 @@ import java.util.UUID;
 public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
 
     Optional<CartItem> findByCartIdAndProductId(UUID cartId, UUID productId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+            DELETE FROM CartItem ci
+            WHERE ci.cart.id = :cartId
+              AND ci.product.id = :productId
+            """)
+    int deleteByCartIdAndProductId(
+            @Param("cartId") UUID cartId,
+            @Param("productId") UUID productId
+    );
+
     void deleteAllByCartId(UUID cartId);
 }
