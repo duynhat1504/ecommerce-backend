37766d1487470f60b9e7242a9146ef6eef3db793
Ho Duy Nhat <hdnhat2005@gmail.com>
2026-08-09 00:14:20 +0700

    feature(redis): thêm sessionId vào blacklist, ko xóa blacklist từng token nhằm để sd các mục đích khác nhau

--- src/main/java/com/duynhat/ecommerce_backend/modules/auth/impl/AccessTokenBlacklistServiceImpl.java
+++ src/main/java/com/duynhat/ecommerce_backend/modules/auth/impl/AccessTokenBlacklistServiceImpl.java
@@ -3,23 +3,29 @@ package com.duynhat.ecommerce_backend.modules.auth.impl;
 import com.duynhat.ecommerce_backend.modules.auth.AccessTokenBlacklistService;
 import com.duynhat.ecommerce_backend.modules.auth.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class AccessTokenBlacklistServiceImpl implements AccessTokenBlacklistService {
 
-    private static final String BLACKLIST_PREFIX = "auth:blacklist:access-token:";
+    private static final String ACCESS_TOKEN_BLACKLIST_PREFIX = "auth:blacklist:access-token:";
+    private static final String SESSION_BLACKLIST_PREFIX = "auth:blacklist:session:";
 
     @Autowired
     private StringRedisTemplate redisTemplate;
 
     @Autowired
     private JwtService jwtService;
+
+    @Value("${jwt.access-token-expiration}")
+    private long accessTokenExpiration;
 
     public AccessTokenBlacklistServiceImpl(StringRedisTemplate redisTemplate, JwtService jwtService) {
         this.redisTemplate = redisTemplate;
@@ -41,7 +47,7 @@ public class AccessTokenBlacklistServiceImpl implements AccessTokenBlacklistServ
         }
 
         redisTemplate.opsForValue().set(
-                BLACKLIST_PREFIX + jti,
+                ACCESS_TOKEN_BLACKLIST_PREFIX + jti,
                 "revoked",
                 ttl
         );
@@ -50,7 +56,33 @@ public class AccessTokenBlacklistServiceImpl implements AccessTokenBlacklistServ
     @Override
     public boolean isBlacklisted(String jti) {
         return Boolean.TRUE.equals(
-                redisTemplate.hasKey(BLACKLIST_PREFIX + jti)
+                redisTemplate.hasKey(ACCESS_TOKEN_BLACKLIST_PREFIX + jti)
+        );
+    }
+
+    @Override
+    public void blacklistSession(UUID sessionId) {
+        if (sessionId == null) {
+            throw new IllegalArgumentException("Session ID must not be null");
+        }
+
+        Duration ttl =Duration.ofMillis(accessTokenExpiration);
+
+        redisTemplate.opsForValue().set(
+                SESSION_BLACKLIST_PREFIX + sessionId,
+                "revoked",
+                ttl
+        );
+    }
+
+    @Override
+    public boolean isSessionBlacklisted(UUID sessionId) {
+        if (sessionId == null) {
+            return false;
+        }
+
+        return Boolean.TRUE.equals(
+                redisTemplate.hasKey(SESSION_BLACKLIST_PREFIX + sessionId)
         );
     }
 }
